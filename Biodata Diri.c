cc05143dcbbcf8f9eac0f4f8022eeabb7d1b2508
ariohartoko <ariohartoko@gmail.com>
2020-09-16 11:44:07 +0700

    biodata pribadi

---
+++ Biodata Diri.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	printf("BIODATA DIRI");
+	printf("\n\n---------------------------------------------------------------------------------------------------\n\n");
+	printf("Nama\t\t\t: Ario Hartoko\n");
+	printf("NPM\t\t\t: 19081010125\n");
+	printf("Jenis Kelamin\t\t: Laki-laki\n");
+	printf("Agama\t\t\t: Kristen Protestan\n");
+	printf("TTl\t\t\t: Bekasi, 12 November 2001\n");
+	printf("Alamat\t\t\t: Surabaya, Jawa Timur, Indonesia\n");
+	printf("Pendidikan Terakhir\t: SMAN 9 Bekasi\n");
+	printf("Pendidikan Saat Ini\t: UPN 'Veteran' Jawa Timur jurusan Teknik Informatika\n");
+	printf("Hobi\t\t\t: Bersepedah, Bermain game, Menonton anime, Membaca komik\n");
+	return 0;
+}
